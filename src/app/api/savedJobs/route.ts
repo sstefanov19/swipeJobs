13aeb64cfc6d128da2a3efe7c5e3157380d2aafa
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2024-10-07 11:35:56 +0300

    removed the post route for testing

--- src/app/api/savedJobs/route.ts
+++ src/app/api/savedJobs/route.ts
@@ -1,15 +1,15 @@
-import { NextRequest, NextResponse } from "next/server";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest ,  res: NextResponse) {
+// export async function POST(req: NextRequest ,  res: NextResponse) {
 
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const body = await req.json();
+//     // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+//     const body = await req.json();
 
-    const {job} = body;
-
-
+//     const {job} = body;
 
 
 
-  return NextResponse.json(body);
-}
+
+
+//   return NextResponse.json(body);
+// }
